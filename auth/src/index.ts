ea567f44e80ef51dcd5f6cb61d4de3564a53bc9d
Stuart Huggins <stuhuggins@gmail.com>
2022-01-24 12:01:51 +0000

    add custom typed errors, use generalized error handling, install express-async-errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,17 +1,26 @@
 import express from 'express';
 import { json } from 'body-parser';
+import 'express-async-errors';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
 
-app.use((req, res, next) => {
-  console.log('received request');
-  next();
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', () => {
+  throw new NotFoundError();
 });
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('hi');
-});
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
