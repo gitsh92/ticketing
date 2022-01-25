fb572d6c716d459fc388b43a5db302ff29822fd4
Stuart Huggins <stuhuggins@gmail.com>
2022-01-25 18:25:22 +0000

    implement full auth flow with jwt and session cookie, create auth middlewares

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import { BadRequestError } from '../errors/bad-request-error';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { validateRequest } from '../middlewares/validate-request';
 import { User } from '../models/user';
 
 const router = express.Router();
@@ -15,12 +16,8 @@ router.post(
       .isLength({ min: 8, max: 20 })
       .withMessage('Password must be between 8 and 20 characters')
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
@@ -28,10 +25,22 @@ router.post(
       throw new BadRequestError('Email in use');
     }
 
-    // hash password
-
     const user = User.build({ email, password });
     await user.save();
+
+    // generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email
+      },
+      process.env.JWT_KEY!
+    );
+
+    // store it on the session object
+    req.session = {
+      jwt: userJwt
+    };
 
     res.status(201).send(user);
   }
