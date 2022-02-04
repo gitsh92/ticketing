bf9b5d20ff7606667c920e7221e316f5c678ce49
Stuart Huggins <stuhuggins@gmail.com>
2022-02-04 18:58:04 +0000

    create orders service, test endpoints with supertest, set up with kubernetes

---
+++ orders/src/routes/delete.ts
@@ -0,0 +1,33 @@
+import { Router, Request, Response } from 'express';
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth
+} from '@gitsh92-tickets/common';
+import { Order, OrderStatus } from '../models/order';
+
+const router = Router();
+
+router.delete(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    res.status(204).send(order);
+  }
+);
+
+export { router as deleteOrderRouter };
